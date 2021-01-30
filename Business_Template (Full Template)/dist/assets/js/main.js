ea14df204690ca89a5f215c0242b75c6fee5b937
abdelmonem88 <abdelmonem1888@gmail.com>
2021-01-31 01:57:49 +0200

    modify

--- Business_Template (Full Template)/dist/assets/js/main.js
+++ Business_Template (Full Template)/dist/assets/js/main.js
@@ -129,14 +129,6 @@
   };
 }));
 
-/*!
-* jquery.counterup.js 1.0
-*
-* Copyright 2013, Benjamin Intal http://gambit.ph @bfintal
-* Released under the GPL v2 License
-*
-* Date: Nov 26, 2013
-*/(function(e){"use strict";e.fn.counterUp=function(t){var n=e.extend({time:400,delay:10},t);return this.each(function(){var t=e(this),r=n,i=function(){var e=[],n=r.time/r.delay,i=t.text(),s=/[0-9]+,[0-9]+/.test(i);i=i.replace(/,/g,"");var o=/^[0-9]+$/.test(i),u=/^[0-9]+\.[0-9]+$/.test(i),a=u?(i.split(".")[1]||[]).length:0;for(var f=n;f>=1;f--){var l=parseInt(i/n*f);u&&(l=parseFloat(i/n*f).toFixed(a));if(s)while(/(\d+)(\d{3})/.test(l.toString()))l=l.toString().replace(/(\d+)(\d{3})/,"$1,$2");e.unshift(l)}t.data("counterup-nums",e);t.text("0");var c=function(){t.text(t.data("counterup-nums").shift());if(t.data("counterup-nums").length)setTimeout(t.data("counterup-func"),r.delay);else{delete t.data("counterup-nums");t.data("counterup-nums",null);t.data("counterup-func",null)}};t.data("counterup-func",c);setTimeout(t.data("counterup-func"),r.delay)};t.waypoint(i,{offset:"100%",triggerOnce:!0})})}})(jQuery);
 //toggle navbar
 const navtoggler = document.querySelector(".nav-toggle");
 const listContainer = document.querySelector(".list-container");
@@ -150,17 +142,17 @@ const homeSection = document.querySelector(".home");
 const nav = document.querySelector(".home nav");
 let HomeHeight = homeSection.getBoundingClientRect().height;
 
-// window.addEventListener("scroll", () => {
-//  if (window.pageYOffset > HomeHeight) {
-//   nav.classList.add("fixed");
-//  } else {
-//   nav.classList.remove("fixed");
-//  }
-// });
+window.addEventListener("scroll", () => {
+ if (window.pageYOffset > HomeHeight) {
+  nav.classList.add("fixed");
+ } else {
+  nav.classList.remove("fixed");
+ }
+});
 
 //libs
 $(document).ready(function () {
- //slider
+ //banner slider
  $(".slider").slick({
   infinite: true,
   slidesToShow: 1,
@@ -169,6 +161,49 @@ $(document).ready(function () {
   autoplaySpeed: 5000,
   prevArrow: ".prev-btn",
   nextArrow: ".next-btn",
+ });
+
+ //testimoinials carousel
+ $(".testimonial-carousel").owlCarousel({
+  autoplay: true,
+  loop: true,
+  margin: 30,
+  nav: false,
+  dots: false,
+  autoplayHoverPause: true,
+  autoplaySpeed: 1000,
+  responsiveClass: true,
+  responsive: {
+   0: {
+    items: 1,
+   },
+   1000: {
+    items: 2,
+   },
+  },
+ });
+
+ //clients carousel
+ $(".clients-carousel").owlCarousel({
+  autoplay: true,
+  loop: true,
+  margin: 50,
+  nav: false,
+  dots: false,
+  autoplayHoverPause: true,
+  autoplaySpeed: 1000,
+  responsiveClass: true,
+  responsive: {
+   0: {
+    items: 1,
+   },
+   600: {
+    items: 2,
+   },
+   1000: {
+    items: 4,
+   },
+  },
  });
 
  //Count to
--- Business_Template (Full Template)/public/js/main.js
+++ Business_Template (Full Template)/public/js/main.js
@@ -11,17 +11,17 @@ const homeSection = document.querySelector(".home");
 const nav = document.querySelector(".home nav");
 let HomeHeight = homeSection.getBoundingClientRect().height;
 
-// window.addEventListener("scroll", () => {
-//  if (window.pageYOffset > HomeHeight) {
-//   nav.classList.add("fixed");
-//  } else {
-//   nav.classList.remove("fixed");
-//  }
-// });
+window.addEventListener("scroll", () => {
+ if (window.pageYOffset > HomeHeight) {
+  nav.classList.add("fixed");
+ } else {
+  nav.classList.remove("fixed");
+ }
+});
 
 //libs
 $(document).ready(function () {
- //slider
+ //banner slider
  $(".slider").slick({
   infinite: true,
   slidesToShow: 1,
@@ -30,6 +30,49 @@ $(document).ready(function () {
   autoplaySpeed: 5000,
   prevArrow: ".prev-btn",
   nextArrow: ".next-btn",
+ });
+
+ //testimoinials carousel
+ $(".testimonial-carousel").owlCarousel({
+  autoplay: true,
+  loop: true,
+  margin: 30,
+  nav: false,
+  dots: false,
+  autoplayHoverPause: true,
+  autoplaySpeed: 1000,
+  responsiveClass: true,
+  responsive: {
+   0: {
+    items: 1,
+   },
+   1000: {
+    items: 2,
+   },
+  },
+ });
+
+ //clients carousel
+ $(".clients-carousel").owlCarousel({
+  autoplay: true,
+  loop: true,
+  margin: 50,
+  nav: false,
+  dots: false,
+  autoplayHoverPause: true,
+  autoplaySpeed: 1000,
+  responsiveClass: true,
+  responsive: {
+   0: {
+    items: 1,
+   },
+   600: {
+    items: 2,
+   },
+   1000: {
+    items: 4,
+   },
+  },
  });
 
  //Count to
